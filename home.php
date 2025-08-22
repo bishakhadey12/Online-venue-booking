6b4361e7af88afa6a55d5f92f548d0a59f039d6f
BISHAKHA DEY <bishakhadey12@gmail.com>
2025-08-22 21:51:36 +0530

    Update home.php

--- home.php
+++ home.php
@@ -3,8 +3,4 @@
 <div class="container mt-5">
     <h1 class="text-center mb-4">Available Venues</h1>
     <div class="row">
-      
-<!-- Bootstrap JS (only if not already added in footer) -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+
